9f599864e2966b72ce2fef161fe7c83a2e02dcdd
robilkot <timur.robilko@gmail.com>
2022-09-06 18:15:26 +0300

    add even check, file creation and 'hello' output

--- PIOIVIS/Sem1/LW 1/82.bat
+++ PIOIVIS/Sem1/LW 1/82.bat
@@ -12,5 +12,30 @@ for /l %%i in (0,1,7) do (
 	)
 )
 
-echo The max num in date is %maxnumber%
-pause
+REM (USE FOR DEBUG) set maxnumber=6 
+
+set /A evencheck=%maxnumber%/2*2
+
+if %evencheck%==%maxnumber% (goto :CASE_%maxnumber%) else (echo Hello && pause && exit)
+
+:CASE_2
+	set filename=two
+	goto CASE_END
+:CASE_4
+	set filename=four
+	goto CASE_END
+:CASE_6
+	set filename=six
+	goto CASE_END
+:CASE_8
+	set filename=eight
+	goto CASE_END
+:CASE_0
+	set filename=zero
+	goto CASE_END
+:CASE_END
+	break>%filename%.txt
+	if exist %filename%.txt (echo File "%filename%" created) else (echo Error creating file)
+	pause
+	exit
+	
