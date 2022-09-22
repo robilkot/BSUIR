b9a446ef0a35f8054f2e81a952408fa780e5667f
robilkot <timur.robilko@gmail.com>
2022-09-22 22:15:04 +0300

    rename lvl2 functions

--- OAiP/Sem1/LW3/6_level2.cpp
+++ OAiP/Sem1/LW3/6_level2.cpp
@@ -7,9 +7,9 @@ float func2(float x) {
 	return (1+2*x*x) * pow(2.7182818284590, x * x);
 }
 
-long double fact(int N)
+long double fact2(int N)
 {
-	if (N > 0) return N * fact(N - 1);
+	if (N > 0) return N * fact2(N - 1);
 	if (N == 0) return 1;
 	if (N < 0) return 0;
 }
@@ -34,7 +34,7 @@ int main2() {
 	for (a; a <= b; a += h, i++) {
 		summ = 0;
 		for (int k=0; k <= n; k++) {
-			summ += (2 * k + 1) * pow(a, 2 * k) / fact(k);
+			summ += (2 * k + 1) * pow(a, 2 * k) / fact2(k);
 		}
 		cout << i << " -> " << func2(a) << "  ->  " << summ <<"  ->  " << abs(func2(a)-summ)  << "\n";
 	}
