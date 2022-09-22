e7f3626b13aaaf3b59ed1af12765fa5fbefabd13
robilkot <timur.robilko@gmail.com>
2022-09-22 22:40:35 +0300

    OAIP LW4 finish 1 level

---
+++ OAiP/Sem1/LW4/6_level1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main1() {
+	const int length = 20;
+	float arr[length];
+	float summ=0;
+
+	cout << "Pls input array elements \n\n";
+	for (int i = 0; i < length; i++) {
+		cin >> arr[i];
+	}
+
+	cout << "\n";
+
+	if (!cin) //numeric check
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Non-numeric!" << "\n";
+		return 0;
+	}
+
+	for (int i = 0; i < length; i++) {
+		summ += arr[i]- (int) arr[i];
+	}
+	cout << "The summ is " << summ; //сумма дробных
+
+	cout << "\n";
+}
