fac836034ec5496a6bf0fea685ddcba3fb2e2a20
Tim Robilko <timur.robilko@gmail.com>
2022-10-21 12:21:56 +0300

    add function for getting vertex from line

--- PIOIVIS/Sem1/RR1/RR1/RR1.cpp
+++ PIOIVIS/Sem1/RR1/RR1/RR1.cpp
@@ -1,36 +1,105 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+
+//#define DEBUG
 
 using namespace std;
 
+// Структура вершины
+// Содержит имя и список инцидентности (СИ)
+// Содержит функцию вывода своего СИ
+//
 struct Vertex
 {
-	vector<int> Incidence;
 	string name;
+	vector<int> Incidence;
 
-	void DisplayIncidence() {
+	void DisplayInfo() {
 		cout << "\n For vertex " << name << ": ";
 		for (int i = 0; i < Incidence.size(); i++) cout << Incidence[i] << " ";
 	}
 };
 
-struct Graph
+// Структура графа
+// Содержит список вершин
+// Содержит функции вывода СИ всех своих вершин
+//
+struct graph
 {
 	vector<Vertex> Vertices;
 
 	void DisplayAllIncidence() {
-		for (int i = 0; i < Vertices.size(); i++) Vertices[i].DisplayIncidence();
+		for (int i = 0; i < Vertices.size(); i++) Vertices[i].DisplayInfo();
 	}
 };
 
+Vertex GetFromLine(string str) { //Парсим строку как запись вершины
+	Vertex Output;
+	size_t pos = 0;
+
+	Output.name = str.substr(0, str.find(" ")); //Парсим имя вершины
+	str.erase(0, pos + Output.name.size() + 1);
+
+	while ((pos = str.find(" ")) != string::npos) { //Парсим список инцидентности
+		int adding;
+		try {
+			adding = stoi(str.substr(0, pos));
+		}
+		catch(...) { //На случай нечислового значения в списке инцидентности
+		#ifdef DEBUG
+		cout << "\nInvalid argument ingnored!\n";
+		#endif // DEBUG
+		str.erase(0, pos + 1 + str.substr(0, pos).size());
+		continue;
+		}
+
+		Output.Incidence.push_back(adding);
+		str.erase(0, pos + 1 + str.substr(0, pos).size());
+	}
+	try {
+		Output.Incidence.push_back(stoi(str)); //И добавляем последний элемент списка
+	}
+	catch (...) {
+	#ifdef DEBUG
+		cout << "\nInvalid argument ingnored!\n";
+	#endif // DEBUG
+	}
+
+	return Output;
+}
+
+graph GetFromFile(const char* filename) {
+	ifstream input(filename);
+	string gotfromfile;
+
+	if (!input.is_open()) {
+		cout << "Error opening file, try again.\n";
+		return {};
+	}
+
+	graph Output;
+
+	string currentline;
+	while (currentline != "") {
+		getline(input, gotfromfile);
+	}
+	
+
+	return Output;
+}
+
 int main() {
-	Vertex first{ {8,2}, "test1" };
+	//Vertex first{ "test1", {8,2} }, second{ "test2", {1,2,4,5}};
+	//graph Gfirst{ {first,second} };
+	//Gfirst.DisplayAllIncidence();
+	
+	//graph inp = ReadFromFile("text.txt");
+	
+	Vertex test = GetFromLine("Vertex1 1 2 3 4 5");
+	test.DisplayInfo();
 
-	Vertex second{ {1,2,4,5}, "test2"};
-
-	Graph Gfirst{{first,second}};
-
-	Gfirst.DisplayAllIncidence();
+	system("pause");
 	return 0;
 }
