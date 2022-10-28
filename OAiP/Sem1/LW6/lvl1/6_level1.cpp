b2227c7172c28b6a983da4f76ec9853673609aaa
robilkot <timur.robilko@gmail.com>
2022-10-28 23:16:44 +0300

    rewrite without string

--- OAiP/Sem1/LW6/lvl1/6_level1.cpp
+++ OAiP/Sem1/LW6/lvl1/6_level1.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
-#include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
 	cout << "\nLW6 level 1\n";
-	string inp;
-	cout << "Pls input string\n\n";
-	cin >> inp;
-	if (size(inp) % 2 == 0 && size(inp) > 4) {
-		cout << "Deleting first and last 2 symbols\n";
-		inp.erase(0, 2);
-		inp.erase(size(inp) - 2, 2);
-		cout << inp;
-	}
-	else {
-		cout << "Less than 4 symbols or not even number";
-	}
+	do {
+		char inp[256];
+		cout << "Pls input string\n\n";
+		cin.getline(inp, 256);
+		if (strlen(inp) % 2 == 0 && strlen(inp) > 3) {
+			cout << "Deleting first and last 2 symbols\n";
+			for (int i = 2; i < strlen(inp); i++) inp[i - 2] = inp[i];
+			inp[strlen(inp) - 4] = '\0';
+			cout << inp << "\n";
+		}
+		else {
+			cout << "Less than 4 symbols or not even number\n";
+		}
+		cout << "Press q to exit\n";
+	} while (_getch() != 'q');
 	return 0;
 }
