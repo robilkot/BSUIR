292506b96ab6110f5a6cf9ae1ccf13fd8032f169
robilkot <timur.robilko@gmail.com>
2022-10-28 23:28:34 +0300

    rewrite without string

--- OAiP/Sem1/LW6/lvl2/6_level2.cpp
+++ OAiP/Sem1/LW6/lvl2/6_level2.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
-#include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
 	cout << "\nLW6 level 2\n";
-	string inp;
-	cout << "Pls input string\n\n";
-	cin >> inp;
-	for (int i = 0; i < size(inp); ) {
-		if (isdigit(inp[i])) {
-			i++;
+	do {
+		char inp[256];
+		cout << "Pls input string\n\n";
+		cin.getline(inp, 256);
+		int i = 0, max = strlen(inp);
+		for (; i < max; i++) {
+			if (isdigit(inp[i]) == 0) {
+				for (int k = i; k < max - 1; k++) inp[k] = inp[k + 1];
+				i--;
+				max--;
+			}
 		}
-		else {
-			inp.erase(i, 1);
-		}
-	}
-	inp == "\0" ? cout << "\nNo number found\n" : cout << "\nThe number is " << inp << "\n";
+		inp[i] = '\0';
+		inp[0] == '\0' ? cout << "\nNo number found\n" : cout << "\nThe number is " << inp << "\n";
+		cout << "Press q to exit\n";
+	} while (_getch() != 'q');
 	return 0;
 }
