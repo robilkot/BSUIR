49eedef08e52c9aa67d9b24e6dd75bee6f29fdc4
robilkot <timur.robilko@gmail.com>
2022-10-28 23:44:55 +0300

    rewrite without string

--- OAiP/Sem1/LW6/lvl3/6_level3.cpp
+++ OAiP/Sem1/LW6/lvl3/6_level3.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
-#include <string>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
 	cout << "\nLW6 level 3\n";
-	string inp;
-	char cut[200] = "";
-	int spacenum = -1;
+	do {
+		char inp[256];
+		cout << "Pls input string\n\n";
+		cin.getline(inp, 256);
+		char cut[256] = "";
+		int spacenum = -1;
 
-	cout << "Pls input string\n";
-	getline(cin, inp);
 
-	if (size(inp) > 15) {
-		for (int i = 0; i < 10; i++) {
-			cut[i] = inp[i + 4];
-		}
-		//cout << "Cut string is " << cut << "\n";
+		if (strlen(inp) > 15) {
+			for (int i = 0; i < 10; i++) {
+				cut[i] = inp[i + 4];
+			}
+			//cout << "Cut string is " << cut << "\n";
 
-		for (int i = size(inp); i > 0; i -= 1) {
-			if (inp[i] == ' ') {
-				spacenum = i;
-				break;
+			for (int i = strlen(inp); i > 0; i -= 1) {
+				if (inp[i] == ' ') {
+					spacenum = i;
+					break;
+				}
+			}
+			if (spacenum != -1) {
+				for (int i = spacenum; i < strlen(inp) - 1; i++) inp[i - spacenum] = inp[i + 1];
+				inp[strlen(inp) - spacenum - 1] = '\0';
+				cout << "\nThe string is: " << cut << inp << "\n";
+			}
+			else {
+				cout << "\nNo spaces found!\n";
 			}
 		}
-		if (spacenum != -1) {
-			inp.erase(0, spacenum);
-		}
 		else {
-			cout << "\nNo spaces found!\n";
-			return 0;
+			cout << "\nLess than 15 symbols!\n";
 		}
-		cout << "\nThe string is: " << cut + inp << "\n";
-	}
-	else {
-		cout << "\nLess than 15 symbols!\n";
-		return 0;
-	}
+		cout << "Press q to exit\n";
+	} while (_getch() != 'q');
 	return 0;
 }
