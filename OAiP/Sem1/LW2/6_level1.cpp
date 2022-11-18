2a3de96d8b15fbda323a2825589c530c4d567719
Tim Robilko <timur.robilko@gmail.com>
2022-11-18 10:42:23 +0300

    add 2nd numeric check without regex, edit 2th lw

--- OAiP/Sem1/LW2/6_level1.cpp
+++ OAiP/Sem1/LW2/6_level1.cpp
@@ -1,20 +1,46 @@
-#include <math.h>
+﻿#include <math.h>
 #include <iostream>
 
 using namespace std;
 
+float input_num() { // Проверка на числовой ввод
+	bool ping = true;
+	int done = 0;
+	float output = 0;
+	do {
+		char check[256];
+		cout << "Enter number (double or integer)\n";
+		cin.getline(check, 256);
+		bool point = false;
+		for (int i = 0; i < strlen(check); i++) {
+			if (isdigit(check[i])) done++;
+			else if (i != 0 && i != (strlen(check) - 1) && check[i] == '.' && point == false) {
+				done++;
+				point = true;
+			}
+			else if (i == 0 && check[i] == '-' && i != (strlen(check) - 1)) {
+				done++;
+			}
+			else {
+				cout << "Error\n";
+				ping = false;
+				break;
+			}
+			if (done == strlen(check)) {
+				ping = true;
+				break;
+			}
+		}
+		output = atof(check);
+		done = 0;
+	} while (ping == false);
+	return output;
+}
+
 int main1() {
 	double x,y,z;
 	cout << "pls input z \n";
-	cin >> z;
-
-	if (!cin) //numeric check
-	{
-		cin.clear();
-		cin.ignore();
-		cout << "Non-numeric!" << "\n";
-		return 0;
-	}
+	z = input_num();
 
 	if (z < 0) x = z, cout << "z<0, calculating x=z \n"; else x = sin(z), cout << "z>=0, calculating x=sin(z) \n";
 
--- OAiP/Sem1/LW2/6_level3.cpp
+++ OAiP/Sem1/LW2/6_level3.cpp
@@ -1,29 +1,57 @@
-#include <math.h>
+﻿#include <math.h>
 #include <iostream>
 
 using namespace std;
 
+float input_num() { // Проверка на числовой ввод
+	bool ping = true;
+	int done = 0;
+	float output = 0;
+	do {
+		char check[256];
+		cout << "Enter number (double or integer)\n";
+		cin.getline(check, 256);
+		bool point = false;
+		for (int i = 0; i < strlen(check); i++) {
+			if (isdigit(check[i])) done++;
+			else if (i != 0 && i != (strlen(check) - 1) && check[i] == '.' && point == false) {
+				done++;
+				point = true;
+			}
+			else if (i == 0 && check[i] == '-' && i != (strlen(check) - 1)) {
+				done++;
+			}
+			else {
+				cout << "Error\n";
+				ping = false;
+				break;
+			}
+			if (done == strlen(check)) {
+				ping = true;
+				break;
+			}
+		}
+		output = atof(check);
+		done = 0;
+	} while (ping == false);
+	return output;
+}
+
 double max(double arg1, double arg2) {
-	if (arg1 > arg2) return arg1; else return arg2;
+	return arg1 > arg2 ? arg1 : arg2;
 }
 
 double min(double arg1, double arg2) {
-	if (arg1 < arg2) return arg1; else return arg2;
+	return arg1 < arg2 ? arg1 : arg2;
 }
 
 int main3() {
 	double x, y, z, m;
 
 	cout << "Pls input x,y,z \n";
-	cin >> x >> y >> z;
-
-	if (!cin) //numeric check
-	{
-		cin.clear();
-		cin.ignore();
-		cout << "Non-numeric!" << "\n";
-		return 0;
-	}
+	x = input_num();
+	y = input_num();
+	z = input_num();
 
 	if (y * z != 0 && x + y + z != 0) {
 		if (max(x + y + z, x / (y * z)) != 0) {
