cd782c47e06166bdc2814d81bcd46f59e9b39776
robilkot <timur.robilko@gmail.com>
2023-03-08 16:52:40 +0300

    add lw6

--- OAiP/Sem2/LW6/LW6.cpp
+++ OAiP/Sem2/LW6/LW6.cpp
@@ -1,8 +1,10 @@
+﻿// Вариант 5 - определить число узлов на каждом уровне дерева
+
 #include <iostream>
 
 struct treeNode
 {
-	treeNode(int content, treeNode* left = nullptr, treeNode* right = nullptr) {
+	treeNode(int content = 0, treeNode* left = nullptr, treeNode* right = nullptr) {
 		this->content = content;
 		this->left = left;
 		this->right = right;
@@ -24,27 +26,38 @@ void deleteAll(treeNode* root)
 	if (right) deleteAll(right);
 }
 
-int nodesCount(treeNode* root, size_t targetLevel = 0, size_t currentLevel = 0)
+size_t nodesCount(treeNode* root, size_t targetLevel = 0, size_t currentLevel = 0)
 {
+	if (!root) return 0;
 
+	return targetLevel == currentLevel ? 1
+		: nodesCount(root->left, targetLevel, currentLevel + 1) + nodesCount(root->right, targetLevel, currentLevel + 1);
+}
+
+void showNodesCount(treeNode* root)
+{
+	size_t currentLevel = 0, currentLevelCount = nodesCount(root, 0);
+
+	do {
+		std::cout << "On level " << ++currentLevel << ": " << currentLevelCount << '\n';
+		currentLevelCount = nodesCount(root, currentLevel);	
+	} while (currentLevelCount != 0);
 }
 
 int main()
 {
-	treeNode* root = new treeNode(1);
-	root->right = new treeNode(1);
-	root->left = new treeNode(1);
-	root->right->right = new treeNode(1);
-	root->right->left = new treeNode(1);
+	treeNode* root = new treeNode; // 0 level (root)
 
-	std::cout << nodesCount(root, 0) << '\n';
-	std::cout << nodesCount(root, 0) << '\n';
-	
-	std::cout << nodesCount(root, 1) << '\n';
-	std::cout << nodesCount(root, 1) << '\n';
-	
-	std::cout << nodesCount(root, 2) << '\n';
-	std::cout << nodesCount(root, 2) << '\n';
+	root->right = new treeNode; // 1st level
+	root->left = new treeNode;
+
+	root->right->right = new treeNode; // 2nd level
+	root->right->left = new treeNode;
+	root->left->right = new treeNode;
+
+	root->right->left->right = new treeNode;  // 3rd level
+
+	showNodesCount(root);
 	
 	deleteAll(root);
 }
