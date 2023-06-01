24f1b2f168dab33e4419d351e8994f14eb83794b
robilkot <timur.robilko@gmail.com>
2023-06-01 22:42:46 +0300

    rewrite queue lab

--- OAiP/Sem2/LW4/LW4.cpp
+++ OAiP/Sem2/LW4/LW4.cpp
@@ -1,151 +1,117 @@
 #include <iostream>
-#include <conio.h>
+#include <string>
 
-struct Queue
-{
-    int info = 0;
+using namespace std;
+
+struct DataNode {
+    int key = 0;
+    string value = "empty";
+};
+
+struct Queue {
+    DataNode dataNode;
     Queue* next = nullptr;
     Queue* prev = nullptr;
 };
 
-Queue* pushBack(Queue* back, int in)
-{
-    Queue* t = new Queue;
-    t->info = in;
-    
-    if (back != nullptr) {
-        t->prev = back;
-        back->next = t;
+void showQueue(Queue* queue) {
+    if (queue == nullptr) {
+        cout << "queue is empty\n";
+        return;
     }
-    
-    return t;
-}
-
-Queue* pushFront(Queue* back, int in)
-{
-    Queue* t = new Queue;
-    t->info = in;
-        
-    if (back == nullptr) {
-        return t;
-    }
-    
-    Queue* front = back;
-    while (front->prev != nullptr)
-            front = front->prev;
-    
-    t->next = front;
-    front->prev = t;
-
-    return back;
-}
-
-void popBack(Queue* back) {
-    if (back != nullptr) {
-        if (back->prev != nullptr) {
-            back->prev->next = nullptr;
-        }
-    delete back;
-    }
-}
-
-void popFront(Queue* back) {
-    Queue* front = back;
-    while (front->next != nullptr)
-        front = front->next;
-
-    if (front != nullptr) {
-        if (front->next != nullptr) {
-            front->next->prev = nullptr;
-        }
-        delete front;
-    }
-}
-
-void viewQueue(Queue* back)
-{
-    std::cout << "Queue:\n";
-    Queue* t = back;
-    while (t != NULL) {
-        std::cout << t->info << "\n";
-        t = t->prev;
-    }
-}
-
-void deleteAll(Queue* back)
-{
-    while (back != NULL) {
-        Queue* t = back;
-        back = back->prev;
-        delete t;
-    }
-}
-
-void swapMaxAndMin(Queue* back)
-{
-    if (back == nullptr) return;
-
-    int max = back->info, min = back->info;
-    Queue *maxptr = back, *minptr = back;
-
-    for (Queue* node = back; node != NULL; node = node->prev) {
-        if (node->info > max) {
-            max = node->info; // find max
-            maxptr = node;
-        }
-        else if (node->info < min) {
-            min = node->info; // find min
-            minptr = node;
+    else {
+        cout << "queue from back to front:\n";
+        while (queue != nullptr) {
+            cout << queue->dataNode.key << " - " << queue->dataNode.value << "\n";
+            queue = queue->prev;
         }
     }
+}
 
-    std::swap(minptr->info, maxptr->info);
+void deleteQueue(Queue*& queue) {
+    Queue* iter = queue;
+    queue = nullptr;
+
+    while (iter != nullptr) {
+        Queue* toDelete = iter;
+        iter = iter->prev;
+        delete toDelete;
+    }
+}
+
+void pushFront(Queue*& queue, DataNode dataNode) {
+    if (queue == nullptr) {
+        queue = new Queue;
+        queue->dataNode = dataNode;
+        return;
+    }
+
+    Queue* iter = queue;
+    while (iter->prev != nullptr) {
+        iter = iter->prev;
+    }
+    iter->prev = new Queue;
+    iter->prev->next = iter;
+    iter->prev->dataNode = dataNode;
+}
+
+void pushBack(Queue*& queue, DataNode dataNode) {
+    if (queue == nullptr) {
+        queue = new Queue;
+        queue->dataNode = dataNode;
+        return;
+    }
+
+    queue->next = new Queue;
+    queue->next->prev = queue;
+    queue->next->dataNode = dataNode;
+
+    queue = queue->next;
+}
+
+DataNode popFront(Queue*& queue) {
+    if (queue == nullptr) return { 0, "empty " };
+
+    Queue* toDelete = queue;
+    while (toDelete->prev != nullptr)
+        toDelete = toDelete->prev;
+   
+    DataNode toReturn = toDelete->dataNode;
+    
+    if (toDelete->next == nullptr) queue = nullptr;
+    else {
+        toDelete->next->prev = nullptr;
+        delete toDelete;
+    }
+    return toReturn;
+}
+
+DataNode popBack(Queue*& queue) {
+    if (queue == nullptr) return { 0, "empty "};
+
+    Queue* toDelete = queue;
+    queue = queue->prev;
+    if(queue) queue->next = nullptr;
+
+    DataNode toReturn = toDelete->dataNode;
+
+    delete toDelete;
+    return toReturn;
 }
 
 int main()
 {
-    Queue* back = nullptr;
-    bool exit = 0;
+    Queue* queue = nullptr;
+    DataNode popTest;
 
-    do {
-        std::cout << "Press q to exit, 1 to push element to front, 2 to push element to back,\n" <<
-            "3 to pop from front, 4 to pop from back, 5 to view queue, 6 to swap max and min in queue\n";
+    for (int i = 0; i < 10; i++) pushBack(queue, { i, to_string(i)});
+    showQueue(queue);
+    for (int i = 0; i < 20; i++) popTest = popBack(queue);
 
-        switch (_getch()) {
-        case 'q': exit = 1; break;
-        case '1': {
-            int pushed = 0;
-            std::cout << "Input new value:\n";
-            std::cin >> pushed;
-            back = pushFront(back, pushed);
-            break;
-        }
-        case '2': {
-            int pushed = 0;
-            std::cout << "Input new value:\n";
-            std::cin >> pushed;
-            back = pushBack(back, pushed);
-            break;
-        }
-        case '3': {
-            popFront(back);
-            break;
-        }
-        case '4': {
-            popBack(back);
-            break;
-        }
-        case '5': {
-            viewQueue(back);
-            break;
-        }
-        case '6': {
-            swapMaxAndMin(back);
-            break;
-        }
-        }
-    } while (!exit);
 
-    deleteAll(back);
-    return 0;
+    for (int i = 0; i < 10; i++) pushBack(queue, { i, to_string(i) });
+    showQueue(queue);
+    for (int i = 0; i < 8; i++) popTest = popFront(queue);
+
+    deleteQueue(queue);
 }
