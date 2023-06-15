0f43aa71c630e612cbb7b7753077d47f4bde6940
robilkot <timur.robilko@gmail.com>
2023-06-15 22:42:30 +0300

    Переписал стек и дек. Опять

--- OAiP/Sem2/LW3/LW3.cpp
+++ OAiP/Sem2/LW3/LW3.cpp
@@ -1,119 +1,74 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-struct DataNode {
-	size_t key = 0;
-	string data = "empty";
+struct StackNode {
+	StackNode* next = nullptr;
+	int data = 0;
 };
 
 struct Stack {
-	Stack* next = nullptr;
-	DataNode dataNode;
+	StackNode* top = nullptr;
+
+	Stack() {}
+	~Stack() {
+		clear();
+	}
+
+	void show() {
+		if (top == nullptr) {
+			cout << "Stack is empty!\n";
+			return;
+		}
+		cout << "Stack:\n";
+		StackNode* it = top;
+		while (it != nullptr) {
+			cout << it->data << "\n";
+			it = it->next;
+		}
+	}
+
+	void clear() {
+		while (top != nullptr) {
+			StackNode* toDelete = top;
+			top = top->next;
+			delete toDelete;
+		}
+	}
+
+	void push(int data) {
+		StackNode* newElement = new StackNode;
+		newElement->data = data;
+		newElement->next = top;
+		top = newElement;
+	}
+
+	int pop() {
+		int toReturn = 0;
+		if (top == nullptr) {
+			cout << "Stack is empty!\n";
+		}
+		else {
+			toReturn = top->data;
+			StackNode* toDelete = top;
+			top = top->next;
+			delete toDelete;
+		}
+		return toReturn;
+	}
+
+	int peek() {
+		int toReturn = 0;
+		if (top == nullptr) {
+			cout << "Stack is empty!\n";
+		}
+		else {
+			toReturn = top->data;
+		}
+		return toReturn;
+	}
+
+	bool empty() {
+		return !top;
+	}
 };
-
-void pushToStack(Stack*& stack, DataNode dataNode) {
-	Stack* newElement = new Stack;
-	newElement->dataNode = dataNode;
-	newElement->next = stack;
-	stack = newElement;
-}
-
-void deleteStack(Stack*& stack) {
-	while (stack != nullptr) {
-		Stack* toDelete = stack;
-		stack = stack->next;
-		delete toDelete;
-	}
-}
-
-DataNode popFromStack(Stack*& stack) {
-	DataNode dataNode;
-	if (stack == nullptr) {
-		cout << "Stack is empty!\n";
-	}
-	else {
-		dataNode = stack->dataNode;
-		Stack* toDelete = stack;
-		stack = stack->next;
-		delete toDelete;
-	}
-	return dataNode;
-}
-
-
-void showStack(Stack* stack) {
-	if (stack == nullptr) {
-		cout << "Stack is empty!\n";
-		return;
-	}
-	cout << "Stack:\n";
-	while (stack != nullptr) {
-		cout << stack->dataNode.key << " - " << stack->dataNode.data << "\n";
-		stack = stack->next;
-	}
-}
-
-void swapMinAndMax(Stack*& stack) {
-	if (stack == nullptr) return;
-
-	Stack* iterStack = stack;
-
-	size_t maxKey = stack->dataNode.key,
-		minKey = stack->dataNode.key;
-
-	Stack* min = stack;
-	Stack* minPrev = stack;
-	Stack* max = stack;
-	Stack* maxPrev = stack;
-
-	while (iterStack->next != nullptr) {
-		if (iterStack->next->dataNode.key > maxKey) {
-			maxKey = iterStack->next->dataNode.key;
-			max = iterStack->next;
-			maxPrev = iterStack;
-		}
-		if (iterStack->next->dataNode.key < minKey) {
-			minKey = iterStack->next->dataNode.key;
-			min = iterStack->next;
-			minPrev = iterStack;
-		}
-
-		iterStack = iterStack->next;
-	}
-
-	if (minPrev != min) minPrev->next = max; else stack = max;
-	if (maxPrev != max) maxPrev->next = min; else stack = min;
-
-	swap(min->next, max->next);
-}
-
-int main()
-{
-	Stack* stack = nullptr;
-
-	pushToStack(stack, { 2, "one" });
-	pushToStack(stack, { 1, "two" });
-	pushToStack(stack, { 8, "three" });
-	pushToStack(stack, { 4, "four" });
-	pushToStack(stack, { 5, "one" });
-	pushToStack(stack, { 6, "two" });
-	pushToStack(stack, { 7, "four" });
-	pushToStack(stack, { 3, "three" });
-
-	//showStack(stack);
-
-	//cout << dataNode.data << "\n";
-
-	showStack(stack);
-
-	swapMinAndMax(stack);
-
-	showStack(stack);
-
-	DataNode popTest;
-	for (int i = 0; i < 10; i++) popTest = popFromStack(stack);
-
-	deleteStack(stack);
-}
--- OAiP/Sem2/LW4/LW4.cpp
+++ OAiP/Sem2/LW4/LW4.cpp
@@ -1,117 +1,201 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-struct DataNode {
-    int key = 0;
-    string value = "empty";
+struct Qnode {
+	int data = 0;
+	Qnode* next = nullptr;
+	Qnode* prev = nullptr;
+
+	Qnode(int data) {
+		this->data = data;
+	}
 };
 
 struct Queue {
-    DataNode dataNode;
-    Queue* next = nullptr;
-    Queue* prev = nullptr;
+	Qnode* begin = nullptr;
+	Qnode* end = nullptr;
+
+	Queue() {}
+
+	void pushBack(int data) {
+		Qnode* pushed = new Qnode(data);
+
+		if (!end) {
+			begin = end = pushed;
+		}
+		else {
+			Qnode* pushed = new Qnode(data);
+			end->next = pushed;
+			pushed->prev = end;
+			end = pushed;
+		}
+	}
+
+	void pushFront(int data) {
+		Qnode* pushed = new Qnode(data);
+
+		if (!begin) {
+			begin = end = pushed;
+		}
+		else {
+			begin->prev = pushed;
+			pushed->next = begin;
+			begin = pushed;
+		}
+	}
+
+	int popBack() {
+		if (!end) {
+			cout << "Empty\n";
+			return 0;
+		}
+		int toReturn = end->data;
+		if (end->prev) end->prev->next = nullptr;
+		else begin = nullptr;
+
+		Qnode* toDelete = end;
+		end = end->prev;
+
+		delete toDelete;
+		return toReturn;
+	}
+
+	int popFront() {
+		if (!begin) {
+			cout << "Empty\n";
+			return 0;
+		}
+		int toReturn = begin->data;
+		if (begin->next) begin->next->prev = nullptr;
+		else end = nullptr;
+
+		Qnode* toDelete = begin;
+		begin = begin->next;
+
+		delete toDelete;
+		return toReturn;
+	}
+
+	void show() {
+		if (!begin) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "Queue from begin to end:\n";
+		Qnode* iter = begin;
+		while (iter) {
+			cout << iter->data << "\n";
+			iter = iter->next;
+		}
+	}
+
+	bool empty() {
+		return !begin;
+	}
+
+	~Queue() {
+		while (begin && end) {
+			cout << "deleted " << begin->data << "\n";
+			Qnode* toDelete = begin;
+			begin = begin->next;
+			delete toDelete;
+		}
+	}
 };
 
-void showQueue(Queue* queue) {
-    if (queue == nullptr) {
-        cout << "queue is empty\n";
-        return;
-    }
-    else {
-        cout << "queue from back to front:\n";
-        while (queue != nullptr) {
-            cout << queue->dataNode.key << " - " << queue->dataNode.value << "\n";
-            queue = queue->prev;
-        }
-    }
+
+void DeleteElement(Queue& queue, Qnode* element) {
+	if (element == queue.begin) {
+		queue.popFront();
+		return;
+	}
+	else if (element == queue.end) {
+		queue.popBack();
+		return;
+	}
+
+	if (element->prev) {
+		element->prev->next = element->next;
+	}
+	if (element->next) {
+		element->next->prev = element->prev;
+	}
+	delete element;
 }
 
-void deleteQueue(Queue*& queue) {
-    Queue* iter = queue;
-    queue = nullptr;
+void SwapElements(Queue& queue) {
+	if (!queue.begin || queue.begin == queue.end) return;
 
-    while (iter != nullptr) {
-        Queue* toDelete = iter;
-        iter = iter->prev;
-        delete toDelete;
-    }
+	Qnode* min = queue.begin;
+	Qnode* max = queue.begin;
+
+	// Находим минимальный и максимальный узлы
+	for (Qnode* iter = queue.begin; iter; iter = iter->next) {
+		if (iter->data < min->data) min = iter;
+		else if (iter->data > max->data) max = iter;
+	}
+
+	min = queue.begin->next->next;
+	max = queue.begin->next;
+
+	// Проверка на то, не являются ли заменяемые элементы крайними
+	if (min == queue.end) queue.end = max;
+	else if (max == queue.end) queue.end = min;
+
+	if (max == queue.begin) queue.begin = min;
+	else if (min == queue.begin) queue.begin = max;
+
+	// Непосредственно замена элементов
+	swap(min->prev, max->prev);
+
+	if (min->prev) min->prev->next = min;
+	if (max->prev) max->prev->next = max;
+
+	swap(min->next, max->next);
+
+	if (min->next) min->next->prev = min;
+	if (max->next) max->next->prev = max;
 }
 
-void pushFront(Queue*& queue, DataNode dataNode) {
-    if (queue == nullptr) {
-        queue = new Queue;
-        queue->dataNode = dataNode;
-        return;
-    }
+int main() {
+	Queue queue;
 
-    Queue* iter = queue;
-    while (iter->prev != nullptr) {
-        iter = iter->prev;
-    }
-    iter->prev = new Queue;
-    iter->prev->next = iter;
-    iter->prev->dataNode = dataNode;
-}
+	for(int i = 1; i <= 9; i++)
+		queue.pushFront(i);
 
-void pushBack(Queue*& queue, DataNode dataNode) {
-    if (queue == nullptr) {
-        queue = new Queue;
-        queue->dataNode = dataNode;
-        return;
-    }
+	//queue.show();
 
-    queue->next = new Queue;
-    queue->next->prev = queue;
-    queue->next->dataNode = dataNode;
+	//Qnode* iter1 = queue.begin, *iter2 = queue.begin;
+	//while(iter1->next)
+	//{
+	//	iter1 = iter1->next;
+	//	iter2 = iter1->next;
 
-    queue = queue->next;
-}
+	//	Qnode* pushed = new Qnode(69);
 
-DataNode popFront(Queue*& queue) {
-    if (queue == nullptr) return { 0, "empty " };
+	//	pushed->prev = iter1;
+	//	pushed->next = iter2;
 
-    Queue* toDelete = queue;
-    while (toDelete->prev != nullptr)
-        toDelete = toDelete->prev;
-   
-    DataNode toReturn = toDelete->dataNode;
-    
-    if (toDelete->next == nullptr) queue = nullptr;
-    else {
-        toDelete->next->prev = nullptr;
-        delete toDelete;
-    }
-    return toReturn;
-}
+	//	iter1->next = pushed;
+	//	iter2->prev = pushed;
 
-DataNode popBack(Queue*& queue) {
-    if (queue == nullptr) return { 0, "empty "};
+	//	iter1 = iter2;
+	//}
 
-    Queue* toDelete = queue;
-    queue = queue->prev;
-    if(queue) queue->next = nullptr;
+	queue.show();
 
-    DataNode toReturn = toDelete->dataNode;
+	//DeleteElement(queue, queue.begin);
+	//DeleteElement(queue, queue.end);
 
-    delete toDelete;
-    return toReturn;
-}
+	//DeleteElement(queue, queue.end->prev->prev);
 
-int main()
-{
-    Queue* queue = nullptr;
-    DataNode popTest;
+	Qnode* iter = queue.begin->next->next->next;
+	SwapElements(queue);
 
-    for (int i = 0; i < 10; i++) pushBack(queue, { i, to_string(i)});
-    showQueue(queue);
-    for (int i = 0; i < 20; i++) popTest = popBack(queue);
+	queue.show();
 
+	//SwapElements(queue, iter, iter->next);
 
-    for (int i = 0; i < 10; i++) pushBack(queue, { i, to_string(i) });
-    showQueue(queue);
-    for (int i = 0; i < 8; i++) popTest = popFront(queue);
-
-    deleteQueue(queue);
+	//queue.show();
 }
