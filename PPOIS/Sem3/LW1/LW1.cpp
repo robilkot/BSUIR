8f5eebe46ea01158f94fdd7111e98e49a9915e3a
Tim Robilko <timur.robilko@gmail.com>
2023-09-01 15:56:35 +0300

    Заготовка ЛР1

---
+++ PPOIS/Sem3/LW1/LW1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "LW1_Set.cpp"
+
+int main() {
+
+	Set test;
+	return 0;
+}
---
+++ PPOIS/Sem3/LW1/LW1_Set.cpp
@@ -0,0 +1,61 @@
+﻿#include "LW1_Set.h"
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+// todo: наследовать класс с одним элементом?
+class Set {
+private:
+	bool isEmpty = true;
+	bool isSingleElement = false;
+
+	char singleElement = 0;
+	vector<Set> elements;
+
+	Set(char element) {
+		this->singleElement = element;
+		isSingleElement = true;
+		isEmpty = false;
+	}
+public:
+	Set(string setNotation) {
+
+	};
+	Set(char* setNotation) {
+
+	};
+
+	bool empty() {
+		return isEmpty;
+	}
+
+	int cardinality() {
+		return isSingleElement ? 1 : elements.size();
+	}
+
+	void push(char element) {
+
+	}
+
+	void pop(char element) {
+
+	}
+
+	bool operator [](char element) {
+
+	}
+
+	Set operator + (const Set& set) const {
+
+	}
+
+	Set operator * (const Set& set) const {
+
+	}
+
+	Set boolean() {
+
+	}
+};
