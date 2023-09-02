6604dace452f619139752abdb146179676b706e0
Tim Robilko <timur.robilko@gmail.com>
2023-09-02 20:16:49 +0300

    LW1 indev

--- PPOIS/Sem3/LW1/LW1.cpp
+++ PPOIS/Sem3/LW1/LW1.cpp
@@ -2,7 +2,10 @@
 #include "LW1_Set.cpp"
 
 int main() {
+	//CantorSet test1;
+	//CantorSet test2('a');
+	CantorSet test3("{a,b,c}");
+	//CantorSet test4("{a,b,{c,d}}");
 
-	Set test;
 	return 0;
 }
--- PPOIS/Sem3/LW1/LW1_Set.cpp
+++ PPOIS/Sem3/LW1/LW1_Set.cpp
@@ -1,61 +1,109 @@
-﻿#include "LW1_Set.h"
-#include <string>
-#include <vector>
+﻿#include <string>
+#include <set>
 
 using std::string;
-using std::vector;
+using std::set;
 
-// todo: наследовать класс с одним элементом?
-class Set {
-private:
-	bool isEmpty = true;
-	bool isSingleElement = false;
-
-	char singleElement = 0;
-	vector<Set> elements;
-
-	Set(char element) {
-		this->singleElement = element;
-		isSingleElement = true;
-		isEmpty = false;
-	}
+class CantorSet {
 public:
-	Set(string setNotation) {
+	bool isEmpty = true;
+	char data = 0;
+	set<CantorSet> elements;
+	
+	void push(CantorSet element) {
+		elements.emplace(element);
+	}
 
+	CantorSet stringToSet(string& setNotation)
+	{
+		CantorSet toReturn; 
+
+		for (int i = 1; i < setNotation.size(); i++) {
+			switch (setNotation[i]) {
+			case '{': {
+				//toReturn.push(setNotation.substr(i));
+				break;
+			}
+			case '}': {
+				//setNotation = setNotation.substr(i+1);
+				return toReturn;
+			}
+			case ',': 
+			case ' ': break;
+			default: {
+				toReturn.elements.emplace(CantorSet(setNotation[i]));
+				break;
+
+			}
+			}
+		}
+		
+		return toReturn;
+	}
+
+	CantorSet() {
+		
 	};
-	Set(char* setNotation) {
-
+	CantorSet(string setNotation) {
+		*this = stringToSet(setNotation);
+	};
+	CantorSet(char singleElement) {
+		this->isEmpty = false;
+		this->data = singleElement;
 	};
 
 	bool empty() {
 		return isEmpty;
 	}
 
-	int cardinality() {
-		return isSingleElement ? 1 : elements.size();
+	int cardinality() const {
+		if (isEmpty) return 0;
+		if(!elements.empty()) return elements.size();
+		return 1;
 	}
 
+	void push(string element) {
+		isEmpty = false;
+	}
 	void push(char element) {
-
+		isEmpty = false;
+		CantorSet toPush(element);
+		elements.emplace(toPush);
 	}
 
-	void pop(char element) {
+	/*void pop(string element) {
 
+	}*/
+
+	//bool operator [](string element) {
+
+	//}
+
+	bool operator > (const CantorSet& set) const {
+		//return cardinality() > set.cardinality();
+		return this->data > set.data;
+	}
+	bool operator < (const CantorSet& set) const {
+		return this->data < set.data;
+	}
+	
+	const CantorSet operator + (const CantorSet& set) {
+		CantorSet result;
+		for (const auto& element : set.elements) {
+			result.elements.emplace(element);
+		}
+		return result;
 	}
 
-	bool operator [](char element) {
-
+	CantorSet operator * (const CantorSet& set) const {
+		CantorSet result;
+		for (const auto& element : set.elements) {
+			if (this->elements.count(element)) result.elements.emplace(element);
+		}
+		return result;
 	}
 
-	Set operator + (const Set& set) const {
+	//CantorSet boolean() {
 
-	}
-
-	Set operator * (const Set& set) const {
-
-	}
-
-	Set boolean() {
-
-	}
+	//}
 };
