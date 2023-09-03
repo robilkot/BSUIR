6ae27db2ab687958bd5eb94e5b90e27356bbf629
robilkot <timur.robilko@gmail.com>
2023-09-03 10:56:10 +0300

    Весь функционал кроме булеана

--- PPOIS/Sem3/LW1/LW1.cpp
+++ PPOIS/Sem3/LW1/LW1.cpp
@@ -4,26 +4,24 @@
 
 using std::string;
 using std::set;
+using std::cout;
+using std::cin;
 
 class CantorSet {
 public:
 	mutable bool isEmpty = true;
 	char data = 0;
+	size_t offset = 0;
 	mutable set<CantorSet> elements;
-	unsigned int offset = 0;
-
-	bool isSingleElement() const {
-		return !isEmpty && !elements.size();
-	}
 
 	bool empty() const {
 		return isEmpty;
 	}
-
-	int cardinality() const {
-		if (isEmpty) return 0;
-		if (!elements.empty()) return elements.size();
-		return 1;
+	bool isSingleElement() const {
+		return !isEmpty && !elements.size();
+	}
+	size_t cardinality() const {
+		return elements.size();
 	}
 
 	void push() const {
@@ -47,8 +45,24 @@ public:
 		CantorSet toDelete(element);
 		auto equals = [&](auto const& element) { return element == toDelete; };
 		std::erase_if(this->elements, equals);
+	
+		if (this->elements.size() == 0) this->isEmpty = true;
+	}
+	void pop(CantorSet toDelete) {
+		auto equals = [&](auto const& element) { return element == toDelete; };
+		std::erase_if(this->elements, equals);
 
 		if (this->elements.size() == 0) this->isEmpty = true;
+	}
+
+	CantorSet boolean() {
+		CantorSet result;
+
+		size_t resultCardinality = pow(2, this->cardinality());
+
+		// Я не хочу рекурсию писать(((
+
+		return result;
 	}
 
 	CantorSet() {};
@@ -88,21 +102,45 @@ public:
 		}
 	};
 
+	string toString() const {
+		string result;
+
+		if (this->isSingleElement()) {
+			result = this->data;
+			return result;
+		}
+		if (this->isEmpty)
+			return "{}";
+
+		result += '{';
+
+		for (const auto& element : this->elements)
+			result += element.toString() + ',';
+
+		result.pop_back(); // erase last comma
+		result += '}';
+
+		return result;
+	}
+
 	bool operator < (const CantorSet& set) const {
 		int cardinality1 = this->cardinality(),
 			cardinality2 = set.cardinality();
 
-		if (cardinality1 != cardinality2)
-			return this->cardinality() < set.cardinality();
+		if (cardinality1 == cardinality2) {
+			if (this->isSingleElement() && set.isSingleElement())
+				return this->data < set.data;
 
-		return this->data * !this->isEmpty < set.data * !set.isEmpty;
+			return this->elements < set.elements;
+		}
+
+		return cardinality1 < cardinality2;
 	}
 	bool operator == (const CantorSet& set) const {
-		return this->elements == set.elements &&
-			this->data == set.data &&
-			this->isEmpty == set.isEmpty;
+		return  this->elements == set.elements &&
+				this->data == set.data &&
+				this->isEmpty == set.isEmpty;
 	}
-
 	bool operator [](string element) {
 		CantorSet toFind(element);
 		return this->elements.count(toFind);
@@ -110,28 +148,18 @@ public:
 	bool operator [](CantorSet element) {
 		return this->elements.count(element);
 	}
-
-	// А копирование надо вообще?
-	//CantorSet& operator = (const CantorSet set) {
-	//	this->elements = set.elements;
-	//	this->isEmpty = set.isEmpty;
-	//	this->offset = set.offset;
-	//	this->data = set.data;
-	//	return *this;
-	//}
-
-	// Операторы не працюють
-	//
-	/*CantorSet operator + (const CantorSet& set) const {
+	bool operator [](const CantorSet element) const {
+		return this->elements.count(element);
+	}
+	
+	CantorSet operator + (const CantorSet& set) const {
 		CantorSet result = *this;
 
-		for (auto it = set.elements.begin(); it != set.elements.end(); it++)
-			result.push(*it);
+		for (const auto& element : set.elements)
+			result.push(element);
 
-		result.isEmpty = this->isEmpty || set.isEmpty;
 		return result;
 	}
-
 	CantorSet& operator += (const CantorSet& set) {
 		for (const auto& element : set.elements)
 			this->push(element);
@@ -139,30 +167,69 @@ public:
 		return *this;
 	}
 
+	CantorSet operator - (const CantorSet& set) const {
+		CantorSet result = *this;
+
+		for (const auto& element : set.elements)
+			result.pop(element);
+
+		return result;
+	}
+	CantorSet& operator -= (const CantorSet& set) {
+		for (const auto& element : set.elements)
+			this->pop(element);
+
+		return *this;
+	}
+
 	CantorSet operator * (const CantorSet& set) const {
 		CantorSet result;
 
-		for (auto it = set.elements.begin(); it != set.elements.end(); it++)
-			if(this->elements.count(*it)) result.elements.emplace(*it);
+		for (const auto& element : this->elements) {
+			if (set[element])
+				result.push(element);
+		}
 
-		result.isEmpty = this->isEmpty && set.isEmpty;
 		return result;
-	}*/
+	}
+	CantorSet& operator *= (const CantorSet& set) {
+		CantorSet temp = *this;
+		*this = CantorSet();
 
-	//CantorSet boolean() { }
+		for (const auto& element : temp.elements) {
+			if (set[element])
+				this->push(element);
+		}
+		
+		return *this;
+	}
+
+	friend std::istream& operator>>(std::istream& cin, CantorSet& set)
+	{
+		string input;
+		cin >> input;
+		set = CantorSet(input);
+		return cin;
+	}
+	friend std::ostream& operator<<(std::ostream& cout, CantorSet& set)
+	{
+		cout << set.toString();
+		return cout;
+	}
 };
 
 int main() {
 	string set;
 
-	set = "{a,{b,c}}";
+	set = "{{b,c},{d,e},a,e}";
 	CantorSet test1(set);
 
-	set = "{a,{b,{c,d}}}";
+	set = "{{b,c},a,d,f}";
 	CantorSet test2(set);
 
-	// Я обязательно выживу
-	//test1 += test2;
+	test1 *= test2;
+	//CantorSet test3 = test1 * test2;
+	cout << test1;
 
 	return 0;
 }
