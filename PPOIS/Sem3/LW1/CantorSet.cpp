58fe1ef1edc9e47ae2157f086bc58bc1d4e51c5a
robilkot <timur.robilko@gmail.com>
2023-09-03 15:08:59 +0300

    Юнит тесты и правки функционала

---
+++ PPOIS/Sem3/LW1/CantorSet.cpp
@@ -0,0 +1,209 @@
+﻿#include "CantorSet.h"
+
+using std::string;
+using std::set;
+
+bool CantorSet::empty() const {
+	return isEmpty;
+}
+bool CantorSet::isSingleElement() const {
+	return !isEmpty && !elements.size();
+}
+size_t CantorSet::cardinality() const {
+	return elements.size();
+}
+
+void CantorSet::clear() {
+	this->isEmpty = true;
+	this->data = 0;
+	this->offset = 0;
+	this->elements.clear();
+}
+
+void CantorSet::push() const {
+	this->elements.emplace(CantorSet());
+	this->isEmpty = false;
+}
+void CantorSet::push(char element) const {
+	this->elements.emplace(CantorSet(element));
+	this->isEmpty = false;
+}
+void CantorSet::push(string element) const {
+	this->elements.emplace(CantorSet(element));
+	this->isEmpty = false;
+}
+void CantorSet::push(CantorSet element) const {
+	this->elements.emplace(element);
+	this->isEmpty = false;
+}
+
+void CantorSet::pop(string element) {
+	CantorSet toDelete(element);
+	auto equals = [&](auto const& element) { return element == toDelete; };
+	std::erase_if(this->elements, equals);
+
+	if (this->elements.size() == 0) this->isEmpty = true;
+}
+void CantorSet::pop(CantorSet toDelete) {
+	auto equals = [&](auto const& element) { return element == toDelete; };
+	std::erase_if(this->elements, equals);
+
+	if (this->elements.size() == 0) this->isEmpty = true;
+}
+
+CantorSet CantorSet::boolean() {
+	CantorSet result;
+
+	size_t resultCardinality = pow(2, this->cardinality());
+
+	// Я не хочу рекурсию писать((( Можно начать с одного элемента и приписывать все рекурсивно?
+
+	return result;
+}
+
+CantorSet::CantorSet() {};
+CantorSet::CantorSet(char element) {
+	this->data = element;
+	this->isEmpty = false;
+};
+CantorSet::CantorSet(string element) {
+	if (element.length() == 1) {
+		*this = CantorSet(element[0]);
+		return;
+	}
+
+	for (int i = 1; i < element.size(); i++, this->offset++) {
+		switch (element[i]) {
+		default: {
+			this->push(element[i]);
+			break;
+		}
+		case '{': {
+			CantorSet subset(element.substr(i));
+			this->push(subset);
+			this->offset += subset.offset;
+			i += subset.offset;
+			break;
+		}
+		case '}': {
+			this->offset++; // needed because cycle increment doesn't work after returning
+			return;
+		}
+		case ',': break;
+		}
+	}
+};
+
+string CantorSet::toString() const {
+	string result;
+
+	if (this->isSingleElement()) {
+		result = this->data;
+		return result;
+	}
+	if (this->isEmpty)
+		return "{}";
+
+	result += '{';
+	for (const auto& element : this->elements)
+		result += element.toString() + ',';
+
+	result.pop_back(); // erase last comma
+	result += '}';
+
+	return result;
+}
+
+bool CantorSet::operator < (const CantorSet& set) const {
+	int cardinality1 = this->cardinality(),
+		cardinality2 = set.cardinality();
+
+	if (cardinality1 == cardinality2) {
+		if (this->isSingleElement() && set.isSingleElement())
+			return this->data < set.data;
+
+		return this->elements < set.elements;
+	}
+
+	return cardinality1 < cardinality2;
+}
+bool CantorSet::operator == (const CantorSet& set) const {
+	return  this->elements == set.elements &&
+		this->data == set.data &&
+		this->isEmpty == set.isEmpty;
+}
+bool CantorSet::operator [](string element) {
+	CantorSet toFind(element);
+	return this->elements.count(toFind);
+}
+bool CantorSet::operator [](CantorSet element) {
+	return this->elements.count(element);
+}
+bool CantorSet::operator [](const CantorSet element) const {
+	return this->elements.count(element);
+}
+
+CantorSet CantorSet::operator + (const CantorSet& set) const {
+	CantorSet result = *this;
+
+	for (const auto& element : set.elements)
+		result.push(element);
+
+	return result;
+}
+CantorSet& CantorSet::operator += (const CantorSet& set) {
+	for (const auto& element : set.elements)
+		this->push(element);
+
+	return *this;
+}
+
+CantorSet CantorSet::operator - (const CantorSet& set) const {
+	CantorSet result = *this;
+
+	for (const auto& element : set.elements)
+		result.pop(element);
+
+	return result;
+}
+CantorSet& CantorSet::operator -= (const CantorSet& set) {
+	for (const auto& element : set.elements)
+		this->pop(element);
+
+	return *this;
+}
+
+CantorSet CantorSet::operator * (const CantorSet& set) const {
+	CantorSet result;
+
+	for (const auto& element : this->elements) {
+		if (set[element])
+			result.push(element);
+	}
+
+	return result;
+}
+CantorSet& CantorSet::operator *= (const CantorSet& set) {
+	CantorSet currentSet = *this;
+	this->clear();
+
+	for (const auto& element : currentSet.elements) {
+		if (set[element])
+			this->push(element);
+	}
+
+	return *this;
+}
+
+std::istream& operator>>(std::istream& cin, CantorSet& set)
+{
+	string input;
+	cin >> input;
+	set = CantorSet(input);
+	return cin;
+}
+std::ostream& operator<<(std::ostream& cout, CantorSet& set)
+{
+	cout << set.toString();
+	return cout;
+}
---
+++ PPOIS/Sem3/LW1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,303 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CantorSet.h"
+
+using std::string;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Microsoft {
+	namespace VisualStudio {
+		namespace CppUnitTestFramework {
+			template <> static std::wstring ToString(const CantorSet& set)
+			{
+				string converted = set.toString();
+				return std::wstring(converted.begin(), converted.end());
+			}
+		}
+	}
+}
+
+namespace CantorSetUnitTest
+{
+	TEST_CLASS(CantorSetCreation)
+	{
+	public:
+		TEST_METHOD(empty)
+		{
+			CantorSet testSet;
+
+			Assert::AreEqual(true, testSet.empty());
+			Assert::AreEqual((size_t) 0, testSet.cardinality());
+		}
+		TEST_METHOD(singleElement)
+		{
+			CantorSet testSet('a');
+
+			Assert::AreEqual((string)"a", testSet.toString());
+			Assert::AreEqual((size_t)0, testSet.cardinality());
+		}
+		TEST_METHOD(multipleElements)
+		{
+			string expectedSet = "{a,b,c}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+			Assert::AreEqual((size_t)3, testSet.cardinality());
+		}
+
+		TEST_METHOD(subset)
+		{
+			string expectedSet = "{a,b,{c,d}}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+			Assert::AreEqual((size_t)3, testSet.cardinality());
+		}
+		TEST_METHOD(multipleSubsets)
+		{
+			string expectedSet = "{a,b,{c,d},{e,f}}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+			Assert::AreEqual((size_t)4, testSet.cardinality());
+		}
+		TEST_METHOD(nestedSubsets)
+		{
+			string expectedSet = "{a,{{e,f}},{d,{c}}}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+			Assert::AreEqual((size_t)3, testSet.cardinality());
+		}
+	};
+
+	TEST_CLASS(CantorSetModification)
+	{
+	public:
+		TEST_METHOD(pushEmpty)
+		{
+			string expectedSet = "{{}}";
+			CantorSet testSet;
+
+			testSet.push();
+
+			Assert::AreEqual(false, testSet.empty());
+			Assert::AreEqual((size_t)1, testSet.cardinality());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+		TEST_METHOD(pushElement)
+		{
+			string expectedSet = "{a,b}";
+			CantorSet testSet;
+
+			testSet.push('a');
+			testSet.push("b");
+
+			Assert::AreEqual(false, testSet.empty());
+			Assert::AreEqual((size_t)2, testSet.cardinality());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+		TEST_METHOD(pushString)
+		{
+			string expectedSet = "{a,b,{c,d}}";
+			CantorSet testSet("{a,b}");
+			
+			testSet.push("{c,d}");
+
+			Assert::AreEqual((size_t)3, testSet.cardinality());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+		TEST_METHOD(pushSet)
+		{
+			string expectedSet = "{a,b,{c,d}}";
+			CantorSet testSet("{a,b}");
+			CantorSet testSet2("{c,d}");
+
+			testSet.push(testSet2);
+
+			Assert::AreEqual((size_t)3, testSet.cardinality());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+
+		TEST_METHOD(popString)
+		{
+			string expectedSet = "{a,b}";
+			CantorSet testSet("{a,b,{c,{e,d}}}");
+
+			testSet.pop("{c,{d,e}}");
+
+			Assert::AreEqual((size_t)2, testSet.cardinality());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+		TEST_METHOD(popElement)
+		{
+			string expectedSet = "{a,b}";
+			CantorSet testSet("{a,b,{c,d}}");
+			CantorSet testSet2("{c,d}");
+
+			testSet.pop(testSet2);
+
+			Assert::AreEqual((size_t)2, testSet.cardinality());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+
+		TEST_METHOD(clear)
+		{
+			string expectedSet = "{}";
+			CantorSet testSet("{a,{b,{c,d}},{e,f}}");
+
+			testSet.clear();
+
+			Assert::AreEqual((size_t)0, testSet.cardinality());
+			Assert::AreEqual(true, testSet.empty());
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+	};
+
+	TEST_CLASS(CantorSetMisc)
+	{
+	public:
+		TEST_METHOD(isSingleElement1)
+		{
+			CantorSet testSet("{}");
+
+			Assert::AreEqual(false, testSet.isSingleElement());
+		}
+		TEST_METHOD(isSingleElement2)
+		{
+			CantorSet testSet("{b,c}");
+
+			Assert::AreEqual(false, testSet.isSingleElement());
+		}
+		TEST_METHOD(isSingleElement3)
+		{
+			CantorSet testSet("d");
+
+			Assert::AreEqual(true, testSet.isSingleElement());
+		}
+
+		TEST_METHOD(isEmpty1)
+		{
+			CantorSet testSet("{}");
+
+			Assert::AreEqual(true, testSet.empty());
+		}
+		TEST_METHOD(isEmpty2)
+		{
+			CantorSet testSet("{a}");
+
+			Assert::AreEqual(false, testSet.empty());
+		}
+		TEST_METHOD(isEmpty3)
+		{
+			CantorSet testSet("{{a,b}}");
+
+			Assert::AreEqual(false, testSet.empty());
+		}
+
+		TEST_METHOD(toString1)
+		{
+			string expectedSet = "{}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+		TEST_METHOD(toString2)
+		{
+			string expectedSet = "{a,b,{c,d}}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+		TEST_METHOD(toString3)
+		{
+			string expectedSet = "{a,{b},{c,{d,e,f}}}";
+			CantorSet testSet(expectedSet);
+
+			Assert::AreEqual(expectedSet, testSet.toString());
+		}
+
+		TEST_METHOD(boolean1)
+		{
+			string expectedSet = "{}";
+
+			CantorSet testSet("{}");
+			CantorSet boolean1 = testSet.boolean();
+
+			Assert::AreEqual(expectedSet, boolean1.toString());
+		}
+	};
+
+	TEST_CLASS(CantorSetOperators)
+	{
+	public:
+		TEST_METHOD(union1)
+		{
+			string expectedSet = "{a,b,d,{c,d},{e,{f,g}}}";
+
+			CantorSet testSet1("{a,b,{c,d}}");
+			CantorSet testSet2("{a,d,{c,d},{e,{f,g}}}");
+
+			testSet1 = testSet1 + testSet2;
+
+			Assert::AreEqual(expectedSet, testSet1.toString());
+		}
+		TEST_METHOD(union2)
+		{
+			string expectedSet = "{a,b,d,{c,d},{e,{f,g}}}";
+
+			CantorSet testSet1("{a,b,{c,d}}");
+			CantorSet testSet2("{a,d,{c,d},{e,{f,g}}}");
+
+			testSet1 += testSet2;
+
+			Assert::AreEqual(expectedSet, testSet1.toString());
+		}
+
+		TEST_METHOD(intersection1)
+		{
+			string expectedSet = "{a,{c,d}}";
+
+			CantorSet testSet1("{a,b,{c,d}}");
+			CantorSet testSet2("{a,d,{c,d},{e,{f,g}}}");
+
+			testSet1 = testSet1 * testSet2;
+
+			Assert::AreEqual(expectedSet, testSet1.toString());
+		}
+		TEST_METHOD(intersection2)
+		{
+			string expectedSet = "{a,{c,d}}";
+
+			CantorSet testSet1("{a,b,{c,d}}");
+			CantorSet testSet2("{a,d,{c,d},{e,{f,g}}}");
+
+			testSet1 *= testSet2;
+
+			Assert::AreEqual(expectedSet, testSet1.toString());
+		}
+
+		TEST_METHOD(difference1)
+		{
+			string expectedSet = "{b}";
+
+			CantorSet testSet1("{a,b,{c,d}}");
+			CantorSet testSet2("{a,d,{c,d},{e,{f,g}}}");
+
+			testSet1 = testSet1 - testSet2;
+
+			Assert::AreEqual(expectedSet, testSet1.toString());
+		}
+		TEST_METHOD(difference2)
+		{
+			string expectedSet = "{b}";
+
+			CantorSet testSet1("{a,b,{c,d}}");
+			CantorSet testSet2("{a,d,{c,d},{e,{f,g}}}");
+
+			testSet1 -= testSet2;
+
+			Assert::AreEqual(expectedSet, testSet1.toString());
+		}
+	};
+}
