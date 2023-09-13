72b902e6114e26db04c65ac092d7daee32e16339
Tim Robilko <timur.robilko@gmail.com>
2023-09-13 22:43:04 +0300

    Правки по ЛР, изменена структура с модулями репо

--- OS/Sem3/LW1/print_arguments.sh
+++ OS/Sem3/LW1/print_arguments.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-echo "$@" | tee $1
+echo "$@"
--- OS/Sem3/LW1/print_files.sh
+++ OS/Sem3/LW1/print_files.sh
@@ -1,3 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
-ls $2/*.$3 | tee $1
+# example: ./print_files.sh output2.txt ../../.. md
+
+output_file=output.txt
+dir=.
+
+if [ $1 ]; then 
+	output_file=$1
+fi
+
+if [ $2 ]; then
+	dir=$2
+fi
+
+
+if [ $3 ]; 
+then
+	for file in `find $dir -maxdepth 1 -type f -name "*$3"`
+	do
+		echo $file;
+	done > $output_file
+else
+	for file in `find $dir -maxdepth 1 -type f ! -name "*.*"`
+	do
+		echo $file;
+	done > $output_file
+fi
