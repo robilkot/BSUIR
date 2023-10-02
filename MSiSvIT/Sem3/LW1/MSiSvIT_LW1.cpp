c043334128d72e83fa2594c3768e31c8004ac2a9
Tim Robilko <timur.robilko@gmail.com>
2023-10-02 18:54:26 +0300

    Код ЛР1

---
+++ MSiSvIT/Sem3/LW1/MSiSvIT_LW1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+
+long long fact(int n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	else {
+		return n * fact(n - 1);
+	}
+}
+
+int main()
+{
+	int M = 0;
+
+	do {
+		cout << "Input number (1,13):\n";
+		cin >> M;
+	} while (M == 0 || M > 13);
+
+	cout << fact(M);
+	return 0;
+}
+
+// Вариант 20
+// Временная сложность O(M)
+// Пространственная O(M)
+// Время работы 2M;
+// Используемая память M + 1
