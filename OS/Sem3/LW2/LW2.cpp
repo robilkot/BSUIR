efb096ae3800a888bc87213dd8cd2fa512331e89
Tim Robilko <timur.robilko@gmail.com>
2023-11-15 12:18:25 +0300

    ЛР по ОС

---
+++ OS/Sem3/LW2/LW2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h> 
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid; /* идентификатор процесса */
+    printf("Пока всего один процесс\n");
+    pid = fork(); /* создание нового процесса */
+    printf("Уже два процесса\n");
+    if (pid == 0) {
+        printf("Это Дочерний процесс, его pid=%d\n", getpid());
+        printf("А pid его Родительского процесса=%d\n", getppid());
+    }
+    else if (pid > 0)
+        printf("Это Родительский процесс pid=%d\n", getpid());
+    else
+        printf("Ошибка вызова fork, потомок не создан\n");
+}
