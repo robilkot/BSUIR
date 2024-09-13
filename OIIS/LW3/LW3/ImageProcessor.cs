4fe7c77a541e4471f306a6c7ba3e8bce9103fe42
robilkot <timur.robilko@gmail.com>
2024-09-13 18:24:13 +0300

    LW3

---
+++ OIIS/LW3/LW3/ImageProcessor.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+
+namespace LW3
+{
+    public static class ImageProcessor
+    {
+#pragma warning disable CA1416
+        public static Bitmap EqualizeBrightness(this Bitmap source, Bitmap target)
+        {
+            var brightness1 = GetBrightness(source);
+            var brightness2 = GetBrightness(target);
+
+            var medium = (brightness1 + brightness2) / 2;
+            var brightDifference = medium - brightness2;
+
+            return BrightnessCorrect(source, -brightDifference);
+        }
+
+        private static Bitmap BrightnessCorrect(Bitmap source, double brightDifference)
+        {
+            Bitmap result = new(source.Width, source.Height);
+
+            for (int x = 0; x < source.Width; x++)
+            {
+                for (int y = 0; y < source.Height; y++)
+                {
+                    var color = source.GetPixel(x, y);
+
+                    var red = (int)Math.Clamp(color.R + brightDifference, 0, 255);
+                    var green = (int)Math.Clamp(color.G + brightDifference, 0, 255);
+                    var blue = (int)Math.Clamp(color.B + brightDifference, 0, 255);
+
+                    var newColor = Color.FromArgb(255, red, green, blue);
+                    result.SetPixel(x, y, newColor);
+                }
+            }
+
+            return result;
+        }
+
+        public static double GetBrightness(Bitmap image)
+        {
+            double brightLevel = 0;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    brightLevel += image.GetPixel(i, j).ToLuminance();
+                }
+            }
+
+            return brightLevel / (image.Width * image.Height);
+        }
+
+        private static double ToLuminance(this Color color) 
+            => (color.R * 0.3) + (color.G * 0.59) + (color.G * 0.11);
+#pragma warning restore CA1416
+    }
+}
---
+++ OIIS/LW3/LW3/Program.cs
@@ -0,0 +1,14 @@
+﻿using LW3;
+using System.Drawing;
+
+#pragma warning disable CA1416
+
+const string sourceFileName = "white";
+const string targetFileName = "black";
+
+Bitmap source = new($"{sourceFileName}.png");
+Bitmap target = new($"{targetFileName}.png");
+
+var processed = source.EqualizeBrightness(target);
+
+processed.Save($"{sourceFileName}_equalized.png");
