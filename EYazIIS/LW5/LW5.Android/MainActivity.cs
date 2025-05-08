d03ecd372e15b9cfbdc21c72b6a0c076b87135c7
robilkot <timur.robilko@gmail.com>
2025-05-08 10:13:26 +0300

    Android project

---
+++ EYazIIS/LW5/LW5.Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Views;
+using Android.Content.PM;
+
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.ReactiveUI;
+
+namespace LW5.Android;
+
+[Activity(
+    Label = "LW5.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    WindowSoftInputMode = SoftInput.AdjustResize,
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .UseReactiveUI();
+    }
+}
